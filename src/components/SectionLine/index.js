d44931acb143e303586a2b27fa7627ef5852dd63
ximbong91023 <ximbong91023@users.noreply.github.com>
2018-07-05 00:11:38 +0300

    Add SectionLine component

---
+++ src/components/SectionLine/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./index.css";
+
+const SectionLine = () => {
+  return (
+    <div className="section-line">
+      <a>New post</a>
+    </div>
+  );
+};
+
+export default SectionLine;
