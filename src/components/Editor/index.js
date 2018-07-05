237c3c110ebe27a7a9d0c347cb02d67ec895338c
Hai Truong <haitruong181096@gmail.com>
2018-07-05 19:40:03 +0300

    Modify Editor component

--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -1,14 +1,32 @@
 import React, { Component } from "react";
 
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+
+import * as types from "../../actions";
+
 import "./index.css";
 
 class Editor extends Component {
   constructor(props) {
     super(props);
+    const id = props.match
+      ? parseInt(props.match.params.id, 10)
+      : this.props.id;
+
+    const data = this.props.displayData && this.props.displayData(id);
+
+    const title = data ? data.title : "";
+    const description = data ? data.description : "";
+    const content = data ? data.content : "";
+    const category = data ? data.category : "home";
+
     this.state = {
-      title: "",
-      category: "",
-      content: ""
+      title,
+      category,
+      description,
+      content,
+      id
     };
   }
 
@@ -19,25 +37,36 @@ class Editor extends Component {
   };
 
   render() {
-    const { title, category, content } = this.state;
+    const { title, description, category, content, id } = this.state;
 
-    console.log(this.state);
+    const action = this.props.action;
 
     return (
       <div className="editor">
         <div className="inputs">
-          <input
-            type="text"
-            placeholder="Title"
-            value={title}
-            onChange={e => this.handle(e, "title")}
-          />
-          <input
-            type="text"
-            placeholder="Category"
-            value={category}
-            onChange={e => this.handle(e, "category")}
-          />
+          <div>
+            <input
+              type="text"
+              placeholder="Title"
+              value={title}
+              onChange={e => this.handle(e, "title")}
+            />
+            <input
+              type="text"
+              placeholder="Description"
+              value={description}
+              onChange={e => this.handle(e, "description")}
+            />
+          </div>
+          <select onClick={e => this.handle(e, "category")}>
+            <option value="home">HOME</option>
+            <option value="future_human">FUTURE HUMAN</option>
+            <option value="mercedes">CULTURE</option>
+            <option value="tech">TECH</option>
+            <option value="entrepreneurship">ENTREPRENEURSHIP</option>
+            <option value="politics">POLITICS</option>
+            <option value="more">MORE</option>
+          </select>
         </div>
         <textarea
           name="name"
@@ -46,7 +75,11 @@ class Editor extends Component {
           onChange={e => this.handle(e, "content")}
         />
         <div className="buttons">
-          <button>Save</button>
+          <Link to={`/post/${id}`}>
+            <button onClick={() => this.props[`${action}Data`](this.state, id)}>
+              Save
+            </button>
+          </Link>
           <button>Cancel</button>
         </div>
       </div>
@@ -54,4 +87,28 @@ class Editor extends Component {
   }
 }
 
-export default Editor;
+const mapStateToProps = state => {
+  return {
+    data: state.data,
+    id: state.id
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addData: (data, id) =>
+      dispatch({
+        type: types.ADD_DATA,
+        data,
+        id
+      }),
+    editData: (data, id) =>
+      dispatch({
+        type: types.EDIT_DATA,
+        data,
+        id
+      })
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Editor);
