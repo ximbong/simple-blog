0a439689f6dcb615191a14fb7d29916b3025ed8d
ximbong91023 <ximbong91023@users.noreply.github.com>
2018-07-08 21:57:52 +0300

    Add category object to sync object with the options and nav titles, instead of manual typing.

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,13 +1,23 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+
 import Post from "../Post";
+import category from "../../category.js";
 
 import "./index.css";
 
-const Main = props => {
-  const data = props.data;
-  const PostList = Object.values(data).map((e, i) => {
-    return <Post data={data[i]} key={i} />;
+const Main = ({ featured_posts }) => {
+  const CategoryButtons = Object.keys(category).map(e => {
+    return (
+      <Link to={`/category/${e}`} key={e}>
+        <div className="category-name">{category[e]}</div>
+      </Link>
+    );
+  });
+
+  const PostList = Object.values(featured_posts).map((e, i) => {
+    return <Post data={featured_posts[i]} key={i} />;
   });
 
   const FirstColumnPost = PostList[0];
@@ -16,15 +26,7 @@ const Main = props => {
 
   return (
     <React.Fragment>
-      <div className="categories">
-        <div className="category-name active">Home</div>
-        <div className="category-name ">Future human</div>
-        <div className="category-name">Culture</div>
-        <div className="category-name">Tech</div>
-        <div className="category-name">Entrepreneurship</div>
-        <div className="category-name">Politics</div>
-        <div className="category-name">More</div>
-      </div>
+      <div className="categories">{CategoryButtons}</div>
       <div className="featured">
         <div className="featured-cols big-size">{FirstColumnPost}</div>
         <div className="featured-cols small-size">{SecondColumnPost}</div>
@@ -73,8 +75,11 @@ const Main = props => {
 
 const mapStateToProps = state => {
   return {
-    data: state.data
+    featured_posts: state.data.featured_posts
   };
 };
 
-export default connect(mapStateToProps, null)(Main);
+export default connect(
+  mapStateToProps,
+  null
+)(Main);
