84c81d0ff4f6b804ca6d0b52326b98cc2b62d9c3
ximbong91023 <ximbong91023@users.noreply.github.com>
2018-07-08 22:55:22 +0300

    Redux devtool

--- src/index.js
+++ src/index.js
@@ -9,7 +9,10 @@ import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <Provider store={store}>
